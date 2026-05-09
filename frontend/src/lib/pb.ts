e76192056b0ccdc80738616981cceecdf2d95f3d
Reverendheat <sharp.brandon@gmail.com>
2026-05-08 22:22:51 -0400

    Fix: PB API Url

--- frontend/src/lib/pb.ts
+++ frontend/src/lib/pb.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(import.meta.env.VITE_PB_URL ?? "http://127.0.0.1:8090");
+const pb = new PocketBase(import.meta.env.VITE_PB_URL ?? window.location.origin);
 
 export default pb;
